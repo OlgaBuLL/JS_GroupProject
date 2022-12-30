41389eedc42d7aecfa078c768fce32002896aa92
OlgaBuLL <108359930+OlgaBuLL@users.noreply.github.com>
2022-12-31 00:06:14 +0100

    first commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,27 @@
+// ----------- MOMENT------------
+// Russia
+const momentTz = require("moment-timezone");
+
+const dateRu = document.querySelector(".date-ru");
+require("moment/locale/ru");
+dateRu.innerHTML = momentTz.tz("Europe/Moscow").format("LLLL");
+
+// Canada
+const dateEn = document.querySelector(".date-en");
+require("moment/locale/en-ca");
+dateEn.innerHTML = momentTz.tz("America/Toronto").format("LLLL");
+
+// Serbia
+const dateSr = document.querySelector(".date-sr");
+require("moment/locale/sr");
+dateSr.innerHTML = momentTz.tz("Europe/Belgrade").format("LLLL");
+
+// Default-Serbia
+const moment = require("moment");
+require("moment/locale/sr");
+
+const dateRS = document.querySelector(".default-serbia");
+let nowRS = moment();
+
+moment.locale("sr");
+dateRS.innerHTML = nowRS.format("LLLL");
